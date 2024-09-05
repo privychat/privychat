b87ca1f4612826816c67fc0b5fffc0d35a853e07
Siddesh <siddeshsankhya@gmail.com>
2024-09-05 19:22:25 +0530

    feat: minot style changes

--- components/chat-window.tsx
+++ components/chat-window.tsx
@@ -17,7 +17,7 @@ const ChatWindow = () => {
         <section className="w-[60%] lg:w-[70%] 2xl:w-[75%]">2</section>
       </section>
 
-      <section className="md:hidden flex h-screen  w-screen  m-auto bg-muted/40 backdrop-blur-md border border-white/20  flex-row gap-2 p-2 overflow-y-hidden">
+      <section className="md:hidden flex h-screen  w-screen  m-auto bg-muted/40 backdrop-blur-md  flex-row gap-2 p-2 overflow-y-hidden">
         <section className="w-full flex flex-col gap-1">
           <div className="bg-black rounded-md flex justify-center items-center py-2">
             <h2
